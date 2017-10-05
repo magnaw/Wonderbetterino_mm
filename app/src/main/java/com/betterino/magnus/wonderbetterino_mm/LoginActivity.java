e6a9f8507cc53531eaf509706739344f920d9ffc
Magnus <magnus_enevoldsen@live.dk>
2017-10-05 15:35:47 +0200

    login med firebase implementeret

--- app/src/main/java/com/betterino/magnus/wonderbetterino_mm/LoginActivity.java
+++ app/src/main/java/com/betterino/magnus/wonderbetterino_mm/LoginActivity.java
@@ -1,30 +1,123 @@
 package com.betterino.magnus.wonderbetterino_mm;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonLogin;
+    private EditText emailText;
+    private EditText passwordText;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
+
+        //layout
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonLogin.setOnClickListener((View.OnClickListener) this);
+
+        emailText = (EditText) findViewById(R.id.emailLogin);
+        passwordText = (EditText) findViewById(R.id.passwordLogin);
 
     }
 
     public void onClick(View v) {
         if(v == buttonLogin){
-            Intent i = new Intent(this, MainMenu.class);
-            startActivity(i);
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            if (email.matches("")) {
+                Toast.makeText(this, "You did not enter a username", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else if (password.matches("")) {
+                Toast.makeText(this, "You did not enter a password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            else {
+                login(email, password);
+            }
+
+//            openMainMenu();
+
+
+        }
+    }
+
+    public void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("", "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void openMainMenu() {
+        Intent i = new Intent(this, MainMenu.class);
+        startActivity(i);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 }
