bd6efd0fe3d0b3ada4490f3fc610f488391a59f1
Mathias <kvisgaard1996@gmail.com>
2017-10-05 14:22:49 +0200

    Tilføjet 4 klasser

---
+++ app/src/main/java/com/betterino/magnus/wonderbetterino_mm/JoinLobby.java
@@ -0,0 +1,13 @@
+package com.betterino.magnus.wonderbetterino_mm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinLobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_lobby);
+    }
+}
